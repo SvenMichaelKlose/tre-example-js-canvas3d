ba93bc83d4f77f95991d00e550913ee1b5424f31
Sven Michael Klose <pixel@copei.de>
2017-06-16 16:15:52 +0200

    Calculate dimensions of plane in 3D space for mapping images and videos on it.

--- draw-face.lisp
+++ draw-face.lisp
@@ -7,23 +7,26 @@
       (ctx.line-to i.px i.py)))
   (ctx.close-path))
 
+(fn pythagorean-distance (x1 y1 z1 x2 y2 z2)
+  (sqrt (number+ (* x1 x2) (* y1 y2) (* z1 z2))))
+
 (fn draw-face-image (ctx face texture)
   (ctx.save)
   (make-face-path ctx face)
   (ctx.clip)
-  (with (v  (face.vertices)
-         x0 (car px)
-         y0 (car py)
-         x1 (cadr px)
-         y1 (cadr py)
-         x2 (caddr px)
-         y2 (caddr py)
-         u0 1280    ; TODO: Oops. Calculate this!
+  (with (v  face.vertices
+         x0 (car v).px
+         y0 (car v).py
+         x1 (cadr v).px
+         y1 (cadr v).py
+         x2 (caddr v).px
+         y2 (caddr v).py
+         u0 (pythagorean-distance (car v).x (car v).y (car v).z (cadr v).x (cadr v).y (cadr v).z)
          u1 0
          u2 0
          v0 0
          v1 0
-         v2 768     ; TODO: Oops. Calculate this!
+         v2 (pythagorean-distance (car v).x (car v).y (car v).z (caddr v).x (caddr v).y (caddr v).z)
          delta   (- (number+ (* u0 v1) (* v0 u2) (* u1 v2)) (* v1 u2) (* v0 u1) (* u0 v2))
          delta-a (- (number+ (* x1 v1) (* v0 x2) (* x1 v2)) (* v1 x2) (* v0 x1) (* x0 v2))
          delta-b (- (number+ (* u0 x1) (* x0 u2) (* u1 x2)) (* x1 u2) (* x0 u1) (* u0 x2))
